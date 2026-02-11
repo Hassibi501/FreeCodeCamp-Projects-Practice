b3f83e32bbf2f76a5580083efce4904a473d132f
Hassieb Hassieb <hassieb.n.h@gmail.com>
2026-02-11 15:38:41 -0800

    Completed and tested all methods for Category class.

--- Build-a-Budget-App.py
+++ Build-a-Budget-App.py
@@ -8,9 +8,11 @@ class Category:
         self.ledger.append({"amount": amount, "description": description})
     
     def withdraw(self, amount, description=""):
-        if True:
+        if self.check_funds(amount) == True:
             self.ledger.append({"amount": amount * -1, "description": description})
+            return True
         else:
+            # print("Withdraw Amount is greater than balance")
             return False
 
     def get_balance(self):
@@ -20,19 +22,30 @@ class Category:
         return balance
 
     def transfer(self, amount, other_category):
-        if True:
+        if self.check_funds(amount) == True:
             self.withdraw(amount, f"Transfer to {other_category.name}`")
             other_category.deposit(amount, f"Transfer from {self.name}")
+            return True
         else:
+            # print("transfer Amount is greater than balance")
             return False
         
-    def check_funds(self):
+    def check_funds(self, amount):
         
-        withdraw_amounts = 0
-        for money in self.ledger:
-            if money["amount"] < 0:
-                withdraw_amounts += money["amount"]
-        return withdraw_amounts
+        # withdraw_amounts = 0
+        # for money in self.ledger:
+        #     if money["amount"] < 0:
+        #         withdraw_amounts += money["amount"]
+        # return withdraw_amounts
+        
+        if self.get_balance() < amount:
+            # print(self.get_balance())
+            # print("false")
+            return False
+        else:
+            # print(self.get_balance())
+            # print("true")
+            return True
         
     def __str__(self):
         results = "transtions\n"
@@ -51,9 +64,7 @@ food = Category("Food")
 
 food.deposit(500, "beef")
 
-food.withdraw(100, "chicken")
-
-food.withdraw(50)
+food.withdraw(500, "chicken")
 
 car =  Category("Car")
 
@@ -61,5 +72,8 @@ car.deposit(500, "engine part")
 
 car.transfer(400, food)
 
-#print(food)
+food.withdraw(401)
 
+car.transfer(200, food)
+
+print(food)
