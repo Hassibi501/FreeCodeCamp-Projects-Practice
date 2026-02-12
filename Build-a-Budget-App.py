31f3fbb195cd6759bc93b8334c157aa1bb626df2
Hassieb Hassieb <hassieb.n.h@gmail.com>
2026-02-11 20:53:39 -0800

    Got the logic down in create_spend_chart function to get the total and current withdraw amount for each category

--- Build-a-Budget-App.py
+++ Build-a-Budget-App.py
@@ -60,7 +60,28 @@ class Category:
         
 
 def create_spend_chart(categories):
-    pass
+    
+    total_withdraw_amount = 0
+    current_withdraw_amount = 0
+    lst = []
+    dic = {}
+    for category in categories:
+        print(f"{category.name}: {category.ledger}")
+        for item in range(len(category.ledger)):
+            #print(category.ledger[item["amount"]]) why cant i do this?
+            print(category.ledger[item])
+
+            dic = category.ledger[item]
+            if dic["amount"] < 0:
+                total_withdraw_amount += dic["amount"]
+                current_withdraw_amount += dic["amount"]
+        if current_withdraw_amount != 0:
+            lst.append(round(current_withdraw_amount, 2))
+        current_withdraw_amount = 0
+    print(lst) 
+    print(round(total_withdraw_amount, 2))
+    
+    
 
 # food = Category("Food")
 
@@ -84,4 +105,11 @@ food.withdraw(10.15, 'groceries')
 food.withdraw(15.89, 'restaurant and more food for dessert')
 clothing = Category('Clothing')
 food.transfer(50, clothing)
-print(food)
+car =  Category("Car")
+
+car.deposit(500, "engine part")
+
+car.transfer(400, food)
+#print(food)
+
+create_spend_chart([food, clothing, car])
